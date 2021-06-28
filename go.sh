0a89f5ae0f7b5767b137b99614b167dea519d223
krudisar <krudisar@gmail.com>
2021-06-28 12:33:11 +0200

    Create go.sh

---
+++ go.sh
@@ -0,0 +1,2 @@
+cd spring-boot-kafka-app/
+mvn spring-boot:run -Dspring-boot.run.arguments="--spring.kafka.consumer.bootstrap-servers=kafka.krdemo.net:9092 --spring.kafka.producer.bootstrap-servers=kafka.krdemo.net:9092"
